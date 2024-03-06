04cd4986309a669901912a23d65d5ec579f5672e
wei <wendyhsu09@gmail.com>
2024-03-06 21:46:36 +0800

    練習建完後台的所以有功能

---
+++ back/que.php
@@ -0,0 +1,31 @@
+<fieldset>
+    <legend>新增問卷</legend>
+    <form action="./api/add_que.php" method="post">
+        <div>
+            <label class="clo">問卷名稱</label>
+            <input type="text" name="subject" id="">
+        </div>
+        <div class="clo" id="opt">
+            選項
+            <input type="text" name="option[]" id="">
+            <input type="button" value="更多" onclick="more()">
+        </div>
+        <div>
+            <input type="submit" value="新增">
+            <input type="reset" value="清空">
+        </div>
+    </form>
+</fieldset>
+<script>
+    function more() {
+        let opt = `
+        <div class="clo">
+            選項
+            <input type="text" name="option[]">
+         
+        </div>
+    `
+
+        $("#opt").before(opt);
+    }
+</script>
