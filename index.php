64083b301b6cf0f8f15374ea6981273008d6dbe3
wei <wendyhsu09@gmail.com>
2024-03-06 22:29:47 +0800

    練習完成前台最新文章人氣文章功能

--- index.php
+++ index.php
@@ -12,13 +12,25 @@ include_once "./api/db.php";
     <link href="./css/css.css" rel="stylesheet" type="text/css">
     <script src="./js/jquery-1.9.1.min.js"></script>
     <script src="./js/js.js"></script>
+    <style>
+        .pop {
+            background: rgba(51, 51, 51, 0.8);
+            color: #FFF;
+            min-height: 100px;
+            width: 300px;
+            position: fixed;
+            display: none;
+            z-index: 9999;
+            overflow: auto;
+            height: 400px;
+            padding: 10px;
+        }
+    </style>
 </head>
 
 <body>
-    <div id="alerr" style="background:rgba(51,51,51,0.8); color:#FFF; min-height:100px; width:300px; position:fixed; display:none; z-index:9999; overflow:auto;">
-        <pre id="ssaa"></pre>
-    </div>
-    <iframe name="back" style="display:none;"></iframe>
+
+
     <div id="all">
         <div id="title">
             <?= date("m月d日 l"); ?> |
